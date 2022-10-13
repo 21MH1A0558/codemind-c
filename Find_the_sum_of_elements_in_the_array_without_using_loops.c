253c4989f1fa0a40f71a092c824c260fd72e4f31
21MH1A0558 <98518439+21MH1A0558@users.noreply.github.com>
2022-10-13 18:22:29 +0530

    #include<stdio.h> int main() {     int size;     int a[size],i,sum=0;     scanf("%d",&size);     for(i=0;i<size;i++)     {         scanf("%d",&a[i]);
        } 
        for(i=0;i<size;i++) 
        { 
            sum=sum+a[i]; 
            
        } 
        printf("%d",sum);
        
    }

--- Find_the_sum_of_elements_in_the_array_without_using_loops.c
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
-{
-    int size; 
+{ 
+    int size;
     int a[size],i,sum=0; 
     scanf("%d",&size); 
-    for(i=0;i<size;i++)
+    for(i=0;i<size;i++) 
     { 
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<size;i++)
-    {
+        scanf("%d",&a[i]); 
+        
+    } 
+    for(i=0;i<size;i++) 
+    { 
         sum=sum+a[i]; 
         
     } 
